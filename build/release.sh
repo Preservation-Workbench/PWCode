6a5cba7a4f8ff13ff99c1f10676ea0d8d9a27b93
Morten Eek <mortenee@gmail.com>
2023-09-28 14:21:41 +0200

    More tar fixes

--- build/release.sh
+++ build/release.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 SCRIPTPATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || echo "$0")")"
+RELEASE_DIR="$(dirname "$SCRIPTPATH")"/build/release/
+PWCODE_DIR=$RELEASE_DIR/PWCode
 
 # shellcheck disable=SC1090
 source "$SCRIPTPATH"/make.sh
-
-PWCODE_REPO="https://github.com/Preservation-Workbench/PWCode"
-PWCODE_BASE_DIR="$(dirname "$SCRIPTPATH")"/build/release/pwcode
-PWCODE_DIR="$PWCODE_BASE_DIR"-"$PWCODE_VERSION"-linux64
 
 if ! [ -x "$(command -v git)" ]; then
 	cecho "$RED" "git command missing. Exiting script.."
@@ -20,14 +18,13 @@ if [ -n "$GITCHECK" ]; then
 	exit 1
 fi
 
-if [ -d "$PWCODE_BASE_DIR" ]; then rm -Rf "$PWCODE_BASE_DIR"; fi
 if [ -d "$PWCODE_DIR" ]; then rm -Rf "$PWCODE_DIR"; fi
-git clone "$PWCODE_REPO" "$PWCODE_DIR"
+git clone "https://github.com/Preservation-Workbench/PWCode" "$PWCODE_DIR"
 
 install_rust
 install_pyapp
 build_pwcode "$PWCODE_DIR"
 
 cd "$PWCODE_DIR" && ./pwcode install
-mv "$PWCODE_DIR" "$PWCODE_BASE_DIR"
-tar -zcf "$SCRIPTPATH"/pwcode-"$PWCODE_VERSION"-linux64.tar.gz "$PWCODE_BASE_DIR"
+cd RELEASE_DIR && tar -zcf pwcode-"$PWCODE_VERSION"-linux64.tar.gz "$PWCODE_DIR"
+rm -Rf "$PWCODE_DIR"
